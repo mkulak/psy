57f34a89cfaf09c33a7845a193d235e1012f99bf
mkulak <kleshney@gmail.com>
2021-11-28 22:06:08 +0100

    working sigil pick

--- src/main/kotlin/Simple.kt
+++ src/main/kotlin/Simple.kt
@@ -1,9 +1,17 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.Image
 
 fun main() {
     window.onload = { event ->
-        document.getElementById("root")?.innerHTML = "<h1>TADA</h1>"
+        val img = document.getElementById("mainImg") as Image
+        val nextBtn = document.getElementById("nextBtn") as HTMLButtonElement
+        nextBtn.onclick = {
+            val picId = (1..4).random()
+            img.src = "sigil${picId}.png"
+            Unit
+        }
         Unit
     }
 }
