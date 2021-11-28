e66e179f485623bf25c46cdaef077a2991f2b61c
mkulak <kleshney@gmail.com>
2021-11-28 22:39:53 +0100

    style with pico.css remove duplicates

--- src/main/kotlin/Simple.kt
+++ src/main/kotlin/Simple.kt
@@ -2,13 +2,18 @@ import kotlinx.browser.document
 import kotlinx.browser.window
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.Image
+import kotlin.random.Random
 
 fun main() {
     window.onload = { event ->
         val img = document.getElementById("mainImg") as Image
         val nextBtn = document.getElementById("nextBtn") as HTMLButtonElement
+        var picId = 0
         nextBtn.onclick = {
-            val picId = (1..4).random()
+            val oldPicId = picId
+            while (picId == oldPicId) {
+                picId = Random.nextInt(1, 5)
+            }
             img.src = "sigil${picId}.png"
             Unit
         }
